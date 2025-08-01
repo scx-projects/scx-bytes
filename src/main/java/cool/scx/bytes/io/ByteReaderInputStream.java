4f0e7dd38b0375f96278e8126c0112dd856ea4c2
scx567888 <scx567888@outlook.com>
2025-08-01 17:34:14 +0800

    dev

---
+++ src/main/java/cool/scx/bytes/io/ByteReaderInputStream.java
@@ -0,0 +1,67 @@
+package cool.scx.bytes.io;
+
+import cool.scx.bytes.IByteReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ByteReaderInputStream extends InputStream {
+
+    private final IByteReader byteReader;
+
+    public ByteReaderInputStream(IByteReader byteReader) {
+        this.byteReader = byteReader;
+    }
+
+    @Override
+    public int read() throws IOException {
+        return byteReader.inputStreamRead();
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        return byteReader.inputStreamRead(b, 0, b.length);
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        return byteReader.inputStreamRead(b, off, len);
+    }
+
+    @Override
+    public byte[] readAllBytes() throws IOException {
+        return byteReader.inputStreamReadNBytes(Integer.MAX_VALUE);
+    }
+
+    @Override
+    public byte[] readNBytes(int len) throws IOException {
+        return byteReader.inputStreamReadNBytes(len);
+    }
+
+    @Override
+    public long transferTo(OutputStream out) throws IOException {
+        return byteReader.inputStreamTransferTo(out);
+    }
+
+    @Override
+    public boolean markSupported() {
+        return true;
+    }
+
+    @Override
+    public void mark(int readlimit) {
+        byteReader.mark();
+    }
+
+    @Override
+    public void reset() {
+        byteReader.reset();
+    }
+
+    @Override
+    public void close() throws IOException {
+        byteReader.close();
+    }
+    
+}
